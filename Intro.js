fc4fc925aa65d8b6321192a2a8bc458247b8e08e
pjaeyoung <devdodose@gmail.com>
2021-02-14 16:19:42 +0900

    feat : Intro 컴포넌트 분리 및 nickname 없을 때만 show 하도록 구현

---
+++ Intro.js
@@ -0,0 +1,38 @@
+export function init() {
+  const $form_nickname = document.querySelector(".form-nickname");
+  const $input_nickname = document.querySelector(".input-nickname");
+  const $icon_smile = document.querySelector(".icon-smile i");
+  const $intro = document.querySelector(".intro");
+  const $main = document.querySelector(".main");
+  let isWink = false;
+
+  $intro.classList.remove("hidden");
+
+  $input_nickname.addEventListener("input", (e) => {
+    if (!isWink && e.target.value.length > 0) {
+      isWink = true;
+      $icon_smile.classList.replace("fa-smile", "fa-smile-wink");
+      return;
+    }
+
+    if (e.target.value.length === 0 && isWink) {
+      isWink = false;
+      $icon_smile.classList.replace("fa-smile-wink", "fa-smile");
+    }
+  });
+
+  $form_nickname.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if ($input_nickname.value.length === 0) {
+      return;
+    }
+    $intro.style.transition = "0.5s linear";
+    $intro.style.transform = "translateY(-1000px)";
+    $intro.style.opacity = "0";
+    setTimeout(() => {
+      $main.classList.remove("hidden");
+    }, 500);
+
+    localStorage.setItem("nickname", JSON.stringify($input_nickname.value));
+  });
+}
--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-(function () {
-  const $form_nickname = document.querySelector(".form-nickname");
-  const $input_nickname = document.querySelector(".input-nickname");
-  const $icon_smile = document.querySelector(".icon-smile i");
-  const $intro = document.querySelector(".intro");
-  const $main = document.querySelector(".main");
-  let isWink = false;
+import * as Intro from "./Intro.js";
 
-  $input_nickname.addEventListener("input", (e) => {
-    if (!isWink && e.target.value.length > 0) {
-      isWink = true;
-      $icon_smile.classList.replace("fa-smile", "fa-smile-wink");
-      return;
-    }
+function init() {
+  const nickname = localStorage.getItem("nickname");
+  if (nickname === null) {
+    Intro.init();
+  }
+}
 
-    if (e.target.value.length === 0 && isWink) {
-      isWink = false;
-      $icon_smile.classList.replace("fa-smile-wink", "fa-smile");
-    }
-  });
-
-  $form_nickname.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if ($input_nickname.value.length === 0) {
-      return;
-    }
-    $intro.style.transition = "0.5s linear";
-    $intro.style.transform = "translateY(-1000px)";
-    $intro.style.opacity = "0";
-    setTimeout(() => {
-      $main.classList.remove("hidden");
-    }, 500);
-
-    localStorage.setItem("nickname", JSON.stringify($input_nickname.value));
-  });
-})();
+init();
