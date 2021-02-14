ca0153be7de0011a1df5d0edb074fc86de1ea274
pjaeyoung <devdodose@gmail.com>
2021-02-14 16:08:26 +0900

    feat: 닉네임 제출 시 전환 애니메이션 추가

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 (function () {
+  const $form_nickname = document.querySelector(".form-nickname");
   const $input_nickname = document.querySelector(".input-nickname");
   const $icon_smile = document.querySelector(".icon-smile i");
+  const $intro = document.querySelector(".intro");
+  const $main = document.querySelector(".main");
   let isWink = false;
 
   $input_nickname.addEventListener("input", (e) => {
@@ -14,5 +17,18 @@
       isWink = false;
       $icon_smile.classList.replace("fa-smile-wink", "fa-smile");
     }
+  });
+
+  $form_nickname.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if ($input_nickname.value.length === 0) {
+      return;
+    }
+    $intro.style.transition = "0.5s linear";
+    $intro.style.transform = "translateY(-1000px)";
+    $intro.style.opacity = "0";
+    setTimeout(() => {
+      $main.classList.remove("hidden");
+    }, 500);
   });
 })();
