ec0cd4be2f792d270c754b9e27b5657d835b66d2
pjaeyoung <devdodose@gmail.com>
2021-02-15 15:00:32 +0900

    feat: 시간별 인사 출력

--- Main.js
+++ Main.js
@@ -1,10 +1,38 @@
-function printGreeting($greet_message, nickname) {
-  $greet_message.textContent = `Good Morning ${nickname}!`;
+function getTime() {
+  return new Date().toTimeString().match(/\d.(?=:)/g);
+}
+
+/**
+ *
+ * @param {number} hours
+ * @returns {string}
+ */
+function getGreetingFromHours(hours) {
+  if (hours >= 5 && hours < 12) {
+    return "Good Morining";
+  }
+
+  if (hours >= 12 && hours < 17) {
+    return "Good Afternoon";
+  }
+
+  if ((hours >= 0 && hours < 5) || (hours >= 17 && hours <= 24)) {
+    return "Good Evening";
+  }
+}
+
+function printGreeting($greet_message, nickname, greeting) {
+  $greet_message.textContent = `${greeting} ${nickname}!`;
 }
 
 export function init(nickname) {
   const $main = document.querySelector(".main");
   $main.classList.remove("hidden");
 
-  printGreeting(document.querySelector(".greet-message"), nickname);
+  const [hours, minutes] = getTime();
+  printGreeting(
+    document.querySelector(".greet-message"),
+    nickname,
+    getGreetingFromHours(parseInt(hours))
+  );
 }
